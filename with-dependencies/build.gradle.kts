b12b81e85f21f6f334744488cccd5fa03a08a1f2
Shawn Cicoria <github@cicoria.com>
2022-03-17 22:22:03 -0400

    fat jar

--- with-dependencies/build.gradle.kts
+++ with-dependencies/build.gradle.kts
@@ -1,5 +1,9 @@
+import org.gradle.jvm.tasks.Jar
+
 plugins {
     java
+    application // enabling the plugin here
+    // kotlin("jvm") version "1.6.0"
     /*
      Apply the java-library-distribution plugin to add support for distributing the source .jar and library .jar files
      so they can be used by JMeter. See the plugin docs at https://docs.gradle.org/current/userguide/java_library_distribution_plugin.html
@@ -7,8 +11,14 @@ plugins {
     `java-library-distribution`
 }
 
+
+
 repositories {
     mavenCentral()
+}
+
+application {
+    mainClass.set("the.path.to.the.MainClass")
 }
 
 val slf4jVersion = "1.7.30" // Make sure to use the version that is bundled with JMeter!
@@ -48,6 +58,37 @@ dependencies {
         exclude(group = "com.fasterxml.jackson.core")
     }
 }
+
+// tasks.create("MyFatJar", Jar::class) {
+//     group = "my tasks" // OR, for example, "build"
+//     description = "Creates a self-contained fat JAR of the application that can be run."
+//     manifest.attributes["Main-Class"] = "[[mainClassPath]]"
+//     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+//     val dependencies = configurations
+//         .runtimeClasspath
+//         .get()
+//         .map(::zipTree)
+//     from(dependencies)
+//     with(tasks.jar.get())
+// }
+
+tasks {
+    val fatJar = register<Jar>("fatJar") {
+        dependsOn.addAll(listOf("compileJava", "processResources")) // We need this for Gradle optimization to work
+        archiveClassifier.set("standalone") // Naming the jar
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        manifest { attributes(mapOf("Main-Class" to application.mainClass)) } // Provided we set it up in the application plugin configuration
+        val sourcesMain = sourceSets.main.get()
+        val contents = configurations.runtimeClasspath.get()
+            .map { if (it.isDirectory) it else zipTree(it) } +
+                sourcesMain.output
+        from(contents)
+    }
+    build {
+        dependsOn(fatJar) // Trigger fat jar creation during build
+    }
+}
+
 
 configurations.all {
     exclude(
